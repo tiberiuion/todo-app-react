01a4e195eba0db681cd16b1826dcdc7e72a53710
Tiberiu Ion <tiberiu.vi@gmail.com>
2024-03-06 17:26:55 +0000

    Add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import "./styles.css"
 
 export default function App() {
-  return "hi";
+  return <form className="new-item-form">
+    <div className="form-row">
+      <label htmlFor="">New item</label>
+      <input type="text" id="item" />
+    </div>
+  </form>
 }
