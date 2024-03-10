9a9e78b95750e5e8c6a771867cb75d1c56d4b56d
Tiberiu Ion <tiberiu.vi@gmail.com>
2024-03-10 09:55:15 +0000

    Add functionality for the input.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react"
 import "./styles.css"
 
 export default function App() {
+  // TODO: look into destructuring
+  // Below we're looking at destructuring, a javascript concept NOT react.
+  // state is immutable. It cannot be changed by accessingit directly. It can only be changed by invoking the setNewItem function.
+  const [newItem, setNewItem] = useState("")
   // this return statement can only return ONE element.
   return (
     // we can use a div but that would add bloat to the markup. 
@@ -9,8 +14,15 @@ export default function App() {
     <form className="new-item-form">
       <div className="form-row">
         <label htmlFor="item">New item</label>
-        <input type="text" id="item" />
+        <input
+          value={newItem}
+          // TODO: Go over the react section again and write some comments.
+          onChange={e => setNewItem(e.target.value)}          
+          type="text"
+          id="item"
+        />
       </div>
+      <button className="btn"> Add </button>
     </form>
     <h1 className="header">Todo list</h1>
     <ul className="list">
